2d8d464a28770cea0dcb143b7f4e7204171f4cc5
Meminbb <kevinbolanos22@gmail.com>
2025-05-13 22:40:31 -0600

    correccion

--- Assets/Scripts/MainScene/Tutorial.cs
+++ Assets/Scripts/MainScene/Tutorial.cs
@@ -1,16 +1,53 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tutorial : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Sprite[] tutorialSprites;   // Tus sprites (4 en total)
+    public GameObject tutorialPanel;   // El panel que contiene la imagen y botones
+    public Image displayImage;         // El componente Image que mostrará los sprites
+
+    private int currentIndex = 0;
+    private bool isOpen = false;
+
     void Start()
     {
-        
+        tutorialPanel.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ToggleTutorial()
     {
-        
+        isOpen = !isOpen;
+        tutorialPanel.SetActive(isOpen);
+
+        if (isOpen)
+        {
+            currentIndex = 0;
+            UpdateImage();
+        }
+    }
+
+    public void NextImage()
+    {
+        if (!isOpen) return;
+
+        currentIndex = (currentIndex + 1) % tutorialSprites.Length;
+        UpdateImage();
+    }
+
+    public void PreviousImage()
+    {
+        if (!isOpen) return;
+
+        currentIndex = (currentIndex - 1 + tutorialSprites.Length) % tutorialSprites.Length;
+        UpdateImage();
+    }
+
+    private void UpdateImage()
+    {
+        if (tutorialSprites.Length > 0 && currentIndex >= 0 && currentIndex < tutorialSprites.Length)
+        {
+            displayImage.sprite = tutorialSprites[currentIndex];
+        }
     }
 }
